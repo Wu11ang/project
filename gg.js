0737f426b8dcdddf90f7f44c186a81e67f652653
rus <example@gmail.com>
2025-12-29 23:04:55 +0500

    tech zadacha

--- gg.js
+++ gg.js
@@ -41,3 +41,96 @@ let employees = ["Ali", "Rus", "Tim"];
 function sayHi() {
   console.log("Hi");
 }
+
+
+const eee = [1, 2 ,3]
+
+console.log(eee[2]);
+
+
+const obj ={2:1, b:2};
+
+console.log(obj[1]);
+
+
+const rus  ={
+    a:1, 
+    c:'b'
+
+};
+
+//rus.a = 1234
+
+//console.log(rus['a']);
+//console.log(rus.a);
+
+
+const b = 'b'
+rus.b =1234
+//console.log(rus);
+
+const eee = [1, 2 ,3];
+eee[10] = '123';
+//console.log(eee[10])
+
+
+
+const rus  ={
+    a:1, 
+    c:'b'
+
+};
+
+
+
+let storeName = 'Rus';
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['rus', 'rus1', 'rus2'],
+    products : {
+        bred: 10,
+        milk: 20
+    },
+    open: true
+};
+
+
+
+
+
+
+alert(1);
+const rus = confirm("are y here ?")
+console.log(rus)
+const answer = prompt("Вам есть ?","")
+console.log(typeof(answer))
+const answer = +prompt("Вам есть ?","")
+console.log((answer+5))
+
+const answer = [];
+answer[0] = prompt("имя ?","");
+answer[1] = prompt("фамилия  ?","");
+answer[2] = prompt("отчество?","");
+document.write(answer);
+console.log(typeof(answer));
+
+const category = 'toys';
+console.log('https://url.com/'+ category)
+console.log(`https://url.com/${category}/32`)
+
+const user ="rus"
+alert(`hi,${user}`);
+
+let incr = -1,
+decr = 0;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+console.log(2+2 ==  4)
+
+
+const ischecked = true
+isclosed =false 
+console.log(ischecked || !iisclosed );
---
+++ script.js
@@ -0,0 +1,22 @@
+
+const numberOfFilms = + prompt("сколько фильмов уже посмотрели?","");
+
+
+
+
+const personalMovieDB ={
+    count: personalMovieDB,
+    movies: { },
+    actors:{ },
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt("Один из последних просмотренных фильмов? ","");
+b = prompt("На сколько оцените его?? ","");
+c = prompt("Один из последних просмотренных фильмов? ","");
+d = prompt("На сколько оцените его? ","");
+personalMovieDB.movies[a]= b, [c] = d;
+
+console.log();
