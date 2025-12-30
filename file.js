979de418e14dd6488751ec13fbf4cd131b9b9cce
rus <example@gmail.com>
2025-12-30 21:38:47 +0500

    logicheskie operatory

--- file.js
+++ file.js
@@ -1,20 +1,52 @@
-const numberOfFilms = +prompt("Сколько фильмов уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt("Один из последних просмотренных фильмов?", "");
+// const b = prompt("На сколько оцените его?", "");
+
+// const c = prompt("Один из последних просмотренных фильмов?", "");
+// const d = prompt("На сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+if (1) {
+    console.log("ok!");
+} else {
+    console.log("error")
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
+const num =60;
 
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("На сколько оцените его?", "");
+if (num < 49) {
+    console.log("error")
+}else if (num >100){ 
+    console.log('много');
+} else {
+    console.log('ok')
+};
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+
+const num =60;
+switch (num) {
+    case 49:
+    console.log('neverno');
+    break;
+    case 100:
+    console.log('neverno');
+    break;case 60:
+    console.log('pravda');
+    break;
+    default:
+        console.log("ne ugadal");
+        break;
+};
